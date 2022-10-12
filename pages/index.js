7ae426d6ee7d27798eba6f6909d385024bdb94cc
vulcan <agrawal.prayag13@gmail.com>
2022-10-12 23:23:32 +0530

    Modified

--- pages/index.js
+++ pages/index.js
@@ -97,7 +97,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1 className={styles.title}>Welcome to Larva Labs!</h1>
+        <h1 className={styles.title}>Welcome to Yuga Labs!</h1>
 
         <p className={styles.description}>
           <Profile />
